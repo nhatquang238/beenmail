f5d34ac0a00a3a21cac823101f34768373aa476f
nhatquang238 <nhatquang238@me.com>
2014-01-23 01:00:58 +0800

    finish prototype mail app behaviors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -74,16 +74,9 @@
 		});
 	};
 
-	var newMailTemplate = '<p>New Mail form to be placed here. Users can choose whom to send to.</p>';
-
-	$('.new-mail, .forward').avgrund({
-		showClose: true,
-		template: newMailTemplate,
-		width: 600,
-		height: 400
-	});
-
 	var $messagePreview = $('.message-preview');
+
+	// update read status behavior when clicked on conversations
 	var changePreview = function (e) {
 		$messagePreview.removeClass('active');
 		var $this = $(this);
@@ -91,9 +84,76 @@
 		if ($this.attr('class').indexOf('new') !== -1) {
 			$this.removeClass('new');
 		}
-
 		$this = null;
 	};
+
+	// update stage when choosing a person to talk
+	var choosePerson = function () {
+		var $this = $(this);
+		if ($this.hasClass('active')) {
+			$this.removeClass('active');
+		} else {
+			$this.addClass('active');
+		}
+		$this = null;
+	}
+
+	var newMailTemplate = _.template($('#new-mail-template').html());
+
+	// change view to new email form view
+	var createNewMail = function () {
+		$('.preview').animo({animation: 'fadeOutLeft', duration: 0.3}, function () {
+			$('.preview').addClass('hide');
+		});
+		$('.messages').animo({animation: 'fadeOutRight', duration: 0.3}, function () {
+			$('.messages').addClass('hide');
+		});
+
+		_.delay(function () {
+			$('.beenmail').prepend(newMailTemplate);
+			$('.new-email-form').animo({animation: 'bounceInUp', duration: 0.5})
+		}, 300);
+	}
+
+	// get receivers, change back to normal conversation view and initiate a blank message
+	var startConversation = function () {
+		// get receivers
+		var receivers = [];
+		$('.people .active figcaption').each(function () {
+			receivers.push($(this).text());
+		});
+
+		// Only use 1st name
+		for (var i = receivers.length - 1; i >= 0; i--) {
+			receivers[i] = receivers[i].split(' ')[0];
+		};
+
+		// bounce new email form out
+		$('.new-email-form').animo({animation: 'bounceOutDown', duration: 0.5}, function () {
+			$('.new-email-form').remove();
+		});
+
+		// return to normal conversation view
+		_.delay(function () {
+			var messagePreviewTemplate = _.template($('#message-preview-template').html());
+			$('.message-preview').removeClass('active');
+			$('.search-mail').after(messagePreviewTemplate({receivers: receivers}));
+
+			var list = '<p><span class="sender">You</span> to  <% _.each(receivers, function (receiver) { %><span class="receiver"><%= receiver %></span> <%});%></p><span class="icon2-cross delete-message"></span>';
+			var messageHead = _.template(list, {receivers: receivers});
+			var newMail = '<div class="new-message message"><div class="mail-head">'+ messageHead + '</div><div class="content"><textarea class="message-content" name="" id="" cols="30" rows="6"></textarea></div><div class="send"><p class="btn btn-primary send-mail">Send</p></div></div>';
+
+			$('.message').addClass('hide');
+			$('.control-btn').after(newMail);
+
+			$('.preview').removeClass('hide');
+			$('.messages').removeClass('hide');
+			$('.preview').animo({animation: 'fadeInLeft', duration: 0.3});
+			$('.messages').animo({animation: 'fadeInRight', duration: 0.3});
+
+			receivers = null;
+		}, 500);
+	}
 
 	// bind events
 	$('.messages').on('click', '.reply, .reply-all' , replyMail);
@@ -101,4 +161,7 @@
 	$('.messages').on('click', '.delete-message', deleteMail);
 	$('.preview').on('click', '.message-preview', changePreview);
 	$('.preview').on('click', '.delete-conversation', deleteMail);
+	$('.beenmail').on('click', 'figure', choosePerson);
+	$('.messages').on('click', '.new-mail', createNewMail);
+	$('.beenmail').on('click', '.start-conversation', startConversation);
 })(jQuery);
